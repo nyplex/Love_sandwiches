c1400202ec73b9a2ee91dca70238ea13f8fa2bb4
Alex Nypels <nypels.alexandre@outlook.com>
2022-02-05 12:05:38 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -59,18 +59,6 @@ def validate_data(values):
         return False
         
     return True
-
-
-def update_sales_worksheet(data):
-    """[Update sales worksheet, add new row with the list data provided]
-
-    Args:
-        data ([list]): [list of int]
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully. \n ")
     
 
 def calculate_surplus_data(sales_row):
@@ -98,28 +86,29 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-
-def update_surplus_worksheet(surplus_data):
-    """[Update surplus worksheet, add new row with the list data provided.]
+def update_worksheet(data, worksheet):
+    """[Receive a list of values to be inserted into a worksheet. Update the relevant worsheet with the data provided.]
 
     Args:
-        surplus_data ([list]): [list of surplus values]
+        data ([list]): [list of value to be insert into the worksheet.]
+        worksheet ([string]): [name of the worksheet to update.]
     """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(surplus_data)
-    print("Surplus worksheet updated successfully.\n")
     
-    
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully.\n")
+
+
 
 def main():
     """[Run all program functions]
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches")
 main()
